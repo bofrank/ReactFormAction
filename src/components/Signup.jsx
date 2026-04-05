c2c276d213fd0c8ad6327d061a513fc786a69ef4
bofrank <bo@bofrank.com>
2026-04-05 19:55:15 +0900

    Using react form actions.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,61 @@
+import { useActionState } from 'react';
+
+import { isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength} from '../util/validation'
+
 export default function Signup() {
+
+  function signupAction(prevFormState, formData) {
+    const email = formData.get('email'); 
+    const password = formData.get('password'); 
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name'); 
+    const lastName = formData.get('last-name'); 
+    const role = formData.get('role'); 
+    const terms = formData.get('terms'); 
+    const acquisitionChannel = formData.getAll('acquisition'); 
+    
+    let errors = [];
+
+    if(!isEmail(email)){
+      errors.push('Invalid email address.')
+    }
+
+    if(!isNotEmpty(password) || !hasMinLength(password, 6)){
+      errors.push('You must provide a password with at least six characters.')
+    }
+
+    if(!isEqualToOtherValue(password, confirmPassword)){
+      errors.push('Passwords do not match.')
+    }
+
+    if(!isNotEmpty(firstName) || !isNotEmpty(lastName)){
+      errors.push('Please both your first and last name.')
+    }
+
+    if(!isNotEmpty(role)){
+      errors.push('Please select a role.')
+    }
+
+    if(!terms){
+      errors.push('You must agree to the TOS.')
+    }
+
+    if(acquisitionChannel.length === 0){
+      errors.push('Please select at least one acquisition channel.')
+    }
+
+    if(errors.length > 0){
+      return { errors: errors };
+    }
+
+    return { errors:null };
+
+  }
+
+  const [formState, formAction] = useActionState(signupAction, { errors: null, });
+
   return (
-    <form>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -84,6 +139,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className='errors'>
+          {formState.errors.map(error => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
